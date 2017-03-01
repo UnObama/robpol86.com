7c3a7b97a80bd91709504b054ba72ac382658ae3
Robpol86 <robpol86@gmail.com>
2017-02-28 21:16:24 -0800

    first draft of script

--- docs/_static/dfree_btrfs.sh
+++ docs/_static/dfree_btrfs.sh
@@ -7,10 +7,20 @@
 # Samba by default uses "df" to get free space of a volume. However with Btrfs
 # that value isn't what the user may expect. Running "btrfs fi usage" instead.
 # Save as (chmod +x): /usr/local/bin/dfree_btrfs
+#
+# Samba usually passed just '.' for $1 and sets $PWD to the volume it's
+# requesting.
 
 set -e  # Exit script if a command fails.
 set -u  # Treat unset variables as errors and exit immediately.
 set -o pipefail  # Exit script if pipes fail instead of just the last program.
 
-# TODO
-df $1 | tail -1 | awk '{print $2" "$4}'
+# First get total size from df.
+total=$(df $1 |tail -1 |awk '{print $2}')
+
+# Then get free (estimated) from btrfs fi usage.
+available=$(btrfs fi usage -k $1 2>/dev/null |grep 'Free (estimated)' |awk '{print $3}')
+available=${available::-6}  # Trim ".00KiB" from end.
+
+# Print.
+echo "$total $available"
