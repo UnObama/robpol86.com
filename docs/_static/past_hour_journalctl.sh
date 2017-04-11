bf1c4e55d7eba314e3c5e84077a1d5dfb4680655
Robpol86 <robpol86@gmail.com>
2017-04-11 16:52:29 -0700

    handling kernel messages

--- docs/_static/past_hour_journalctl.sh
+++ docs/_static/past_hour_journalctl.sh
@@ -11,20 +11,34 @@ set -o pipefail  # Exit script if pipes fail instead of just the last program.
 # Iterate journalctl lines.
 journalctl -o json "$@" |while read -r line; do
     declare -A json  # Scoped in piped while loop.
+    comm=
     json=()  # Clear array in case "shopt -s lastpipe" is set by the user.
+    message=
 
     # Read JSON into bash associative array.
     while IFS="=" read -r key value; do
-        json["$key"]="$value"
+        if [ "$key" == "MESSAGE" ]; then
+            message="$value"
+        elif [ "$key" == "_COMM" ]; then
+            comm="$value"
+        else
+            json["$key"]="$value"
+        fi
     done < <(jq -r "to_entries|map(\"\(.key)=\(.value)\")|.[]" <<< "$line")
 
     # Filter influxdb statements.
-    if [ "${json['CONTAINER_NAME']:-}" == "influxdb" ] && [ "${json['MESSAGE']:0:3}" == "[I]" ]; then
+    if [ "${json['CONTAINER_NAME']:-}" == "influxdb" ] && [ "${message:0:3}" == "[I]" ]; then
         continue
+    fi
+
+    # Handle comm column.
+    if [ -z "$comm" ]; then
+        comm="${json['SYSLOG_IDENTIFIER']}"
+    else
+        comm+="[${json['_PID']}]"
     fi
 
     # Print.
     echo -n $(date -d @${json['__REALTIME_TIMESTAMP']:0:-6} '+%b %d %T')
-    echo -n " ${json['_HOSTNAME']} ${json['_COMM']}[${json['_PID']}]:"
-    echo " ${json['MESSAGE']}"
+    echo " ${json['_HOSTNAME']} $comm: $message"
 done
